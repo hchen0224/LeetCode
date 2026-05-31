16521c8c1af03fc74b4725174d4205a4b3f69b3b
chenhao <hchen0224@icloud.com>
2026-05-31 18:21:32 +0800

    2026/05/31

--- src/List/Solution19.java
+++ src/List/Solution19.java
@@ -1,9 +1,18 @@
 package List;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 19. 删除链表的倒数第 N 个结点
  */
 public class Solution19 {
+    /**
+     * 解法1：O(3n)时间复杂度
+     * @param head
+     * @param n
+     * @return
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null){
             return head;
@@ -29,4 +38,73 @@ public class Solution19 {
         }
         return pre;
     }
+
+    /**
+     * 解法2，O(n)时间复杂度，O(n)空间复杂度
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        if (head == null) {
+            return head;
+        }
+
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode cur = head;
+        int index = 0;
+        int count = 0;
+        while (cur != null) {
+            map.put(index, cur);
+            cur = cur.next;
+            ++count;
+            ++index;
+        }
+
+        int indexToDelete = count - n;
+
+        ListNode nodeToDelete = map.get(indexToDelete);
+        ListNode pre = map.get(indexToDelete - 1);
+
+
+        if (pre == null) {
+            return nodeToDelete.next;
+        } else {
+            pre.next = map.get(indexToDelete + 1);
+        }
+
+        return head;
+
+    }
+
+    /**
+     * 解法3：最优解法，双指针，O(n)时间复杂度，O(1)空间复杂度
+     */
+    public ListNode removeNthFromEnd3(ListNode head, int n) {
+        if (head == null ) {
+            return null;
+        }
+        ListNode dummyNode = new ListNode(0, head);
+        ListNode slow = dummyNode;
+        ListNode fast = dummyNode;
+
+        //fast 先走 n 步
+        for (int i = 0; i < n; ++i) {
+            fast = fast.next;
+        }
+
+        //slow跟着fast同步走，直到 fast 走到末尾节点
+        // 此时：
+        // 1.fast在倒数第 1 个位置
+        // 2.fast 比 slow 多走 n 步，
+        // 得出：3.slow 在整个链表的倒数第 n+1 个（即被删除节点的前一个）
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummyNode.next;
+    }
+
 }
--- src/List/Solution2.java
+++ src/List/Solution2.java
@@ -30,4 +30,76 @@ public class Solution2 {
         }
         return head;
     }
+
+    /**
+     * 解法2
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        boolean addOne = false;
+        ListNode dummyNode = new ListNode(0);
+        ListNode cur = dummyNode;
+
+        while (l1 != null && l2 != null) {
+            if (addOne) {
+                l1.val = l1.val + 1;
+                if (l1.val == 10) {
+                    l1.val = 0;
+                } else {
+                    addOne = false;
+                }
+            }
+
+            int nextval = l1.val + l2.val;
+
+            if (nextval >= 10) {
+                addOne = true;
+                nextval = nextval - 10;
+            }
+
+            cur.next = new ListNode(nextval);
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if (l1 != null) {
+            cur.next = connectLeftOver(l1, addOne);
+        } else if (l2 != null) {
+            cur.next = connectLeftOver(l2, addOne);
+        } else {
+            if (addOne) {
+                cur.next = new ListNode(1);
+            }
+        }
+
+        return dummyNode.next;
+
+    }
+
+    private ListNode connectLeftOver(ListNode head, boolean addOne) {
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (addOne) {
+                cur.val += 1;
+                if (cur.val == 10) {
+                    cur.val = 0;
+                } else {
+                    addOne = false;
+                }
+            }
+            cur = cur.next;
+        }
+        if (addOne) {
+            cur.val += 1;
+            if (cur.val == 10) {
+                cur.val = 0;
+                cur.next = new ListNode(1);
+            }
+
+        }
+        return head;
+    }
 }
